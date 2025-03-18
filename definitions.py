60379b00a4c5436b126efa3c3f52af76a07501f1
taiMorioka <thetaimorioka@gmail.com>
2025-03-18 15:07:11 -0700

    added github

---
+++ definitions.py
@@ -0,0 +1,30 @@
+class EventType:
+    BUTTON = 1
+    AXIS = 3
+
+class ButtonEvent:
+    PRESSED = 1
+    RELEASED = 0
+
+class ButtonCode:
+    X = 304
+    CIRCLE = 305
+    TRIANGLE = 307
+    SQUARE = 308
+    LEFT_BUMPER = 310
+    RIGHT_BUMPER = 311
+    LEFT_TRIGGER = 312
+    RIGHT_TRIGGER = 313
+    SHARE = 314
+    OPTIONS = 315
+    PLAYSTATION = 316
+    LEFT_STICK_PUSH = 317
+    RIGHT_STICK_PUSH = 318
+
+class AxisCode:
+    LEFT_STICK_X = 0
+    LEFT_STICK_Y = 1
+    LEFT_TRIGGER = 2
+    RIGHT_STICK_X = 3
+    RIGHT_STICK_Y = 4
+    RIGHT_TRIGGER = 5
---
+++ main.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button)
+from definitions import (ButtonEvent, AxisCode, EventType, ButtonCode)
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
+
+import struct
+
+# Declare motors 
+fl_motor = Motor(Port.A)
+fr_motor = Motor(Port.B)
+bl_motor = Motor(Port.C)
+br_motor = Motor(Port.D)
+
+# Defining variables that will store the dc values for the motors
+drive = 0
+drive_speed = 0
+steer = 0
+# arm = 0
+
+# Arm speed is a value from 0-1 that is multiplied by the input value for the arm movement.
+# 1 is default, but you can lower it for better control
+# arm_speed = 1
+
+#############
+# Drivebase #
+#############
+
+# A drivebase allows for precise and easy control of the robot while acting autonomously
+# Read drivebase documentation linked in the README on github to correctly set the last value
+# robot = DriveBase(left_motor, right_motor, 55.5, 121.5)
+
+# Drivebase settings
+# Arguments: straight speed: mm/s, straight acceleration: mm/s^2
+# turn rate: deg/s, turn acceleration: deg/s^2
+# robot.settings(500, 1000, 100, 100)
+
+########
+# Auto #
+########
+
+
+#########
+# Setup #
+#########
+
+# This function scales a range of values from source_range (typically 0-255) to target_range (for instance -100-100),
+# where 0 is -100, 255 is 100, and all inbetween values are scaled porportionally
+# example: scale(source(0,255), (-100-100))
+def scale(source, source_range, target_range):
+    return (float(source-source_range[0]) / (source_range[1]-source_range[0])) * (target_range[1]-target_range[0])+target_range[0]
+
+# Open the Gamepad event file:
+
+infile_path = "/dev/input/event4"
+in_file = open(infile_path, "rb")
+
+# Read from the file
+FORMAT = 'llHHI'    
+EVENT_SIZE = struct.calcsize(FORMAT)
+
+#############
+# Main loop #
+#############
+
+
+while True: 
+    event = in_file.read(EVENT_SIZE)
+    (_, _, ev_type, code, value) = struct.unpack(FORMAT, event)
+
+    if ev_type == EventType.AXIS:
+        # print(code)
+        if code == 1:
+            print(code, ev_type, value)
+            drive_speed = scale(value, (0,255), (100,-100))
+            print(drive_speed)
+    
+    # Read analog stick values    
+    # if ev_type == 3: # Stick or trigger moved
+    #     if code == 1: # Left stick y axis
+    #         drive = scale(value, (0,255), (100,-100)) # Scale input from 0-255 to -100-100 for the motor
+    #     if code == 3: # Right stick x axis
+    #         steer = scale(value, (0,255), (100, -100)) # Scale input from 0-255 to -100-100 for the motor
+    #     if code == 2: # Left trigger axis
+    #         arm = value / 2
+    #     if code == 5: # Left trigger axis
+    #         arm = -value / 2
+    # if ev_type == 1: # Button pressed
+    #     if code == 310 and value == 1:
+    #         print("L1 Pressed!")
+    #     if code == 310 and value == 0:
+    #         print("L1 Released")
+        
+
+    fl_motor.dc(drive_speed)
+    fr_motor.dc(drive_speed)
+    bl_motor.dc(drive_speed)
+    br_motor.dc(drive_speed)
+    
+
+in_file.close()
